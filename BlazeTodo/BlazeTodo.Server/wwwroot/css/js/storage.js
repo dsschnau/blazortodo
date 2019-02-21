e6d9aad6a9c602bfb3d88ffca3ad58036b09e627
Dan Schnau <dsschnau@gmail.com>
2019-02-21 00:11:35 -0500

    initial commit

---
+++ BlazeTodo/BlazeTodo.Server/wwwroot/css/js/storage.js
@@ -0,0 +1,7 @@
+﻿window.SaveTodos = (todos) => {
+    window.localStorage.setItem('todos', todos);
+}
+
+window.LoadTodos = () => {
+    return window.localStorage.getItem('todos');
+}
---
+++ TodoEntities/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TodoEntities
+{
+    public class Todo : IEquatable<Todo>
+    {
+        public string Name { get; set; }
+
+        public bool Equals(Todo other)
+        {
+            return other == this;
+        }
+    }
+}
